df314874762c5a520d75a05b9bdc73ac3ae3c248
Sancha-aesthetic <joseantonio.sanchachausal@educa.madrid.org>
2026-06-04 16:22:25 +0200

    cambios back

--- src/test/java/es/jadafit/jadafit_api/service/UserServiceTest.java
+++ src/test/java/es/jadafit/jadafit_api/service/UserServiceTest.java
@@ -5,8 +5,8 @@ import es.jadafit.jadafit_api.dto.UserRegistrationDTO;
 import es.jadafit.jadafit_api.exception.ConflictException;
 import es.jadafit.jadafit_api.exception.UnauthorizedException;
 import es.jadafit.jadafit_api.model.User;
-import es.jadafit.jadafit_api.service.EmailService;
 import es.jadafit.jadafit_api.repository.UserRepository;
+import es.jadafit.jadafit_api.repository.UserSessionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,9 +23,15 @@ import static org.mockito.Mockito.when;
 class UserServiceTest {
 
     private final UserRepository userRepository = mock(UserRepository.class);
+    private final UserSessionRepository sessionRepository = mock(UserSessionRepository.class);
     private final PasswordEncoder passwordEncoder = mock(PasswordEncoder.class);
     private final EmailService emailService = mock(EmailService.class);
-    private final UserService userService = new UserService(userRepository, passwordEncoder, emailService);
+    private final UserService userService = new UserService(
+            userRepository,
+            sessionRepository,
+            passwordEncoder,
+            emailService
+    );
 
     @Test
     void registerUserNormalizesEmailAndUsername() {
